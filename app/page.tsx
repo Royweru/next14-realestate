8501bd59957016be9b5464730a83c353b0bd6394
Royweru <royezi17@gmail.com>
2024-02-03 20:58:58 +0300

    modified:   app/layout.tsx 	modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
       <h1 className=" text-3xl font-bold font-serif">
-         
+         APARTAMENTI
       </h1>
     </main>
   );
