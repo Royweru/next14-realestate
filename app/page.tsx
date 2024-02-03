d8ab7d59c921d034e03b57d5a074de2d2bf1e566
Royweru <royezi17@gmail.com>
2024-02-03 20:40:32 +0300

    new file:   .eslintrc.json 	new file:   .gitignore 	new file:   app/favicon.ico 	new file:   app/globals.css 	new file:   app/layout.tsx 	new file:   app/page.tsx 	new file:   components/navigation/navbar.tsx 	new file:   next.config.mjs 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   prisma/schema.prisma 	new file:   public/next.svg 	new file:   public/vercel.svg 	new file:   tailwind.config.ts 	new file:   tsconfig.json

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+      <h1 className=" text-3xl font-bold font-serif">
+         APARTAMENTI
+      </h1>
+    </main>
+  );
+}
---
+++ components/navigation/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className=' w-full rounded-md p-2 flex justify-center items-center py-4'>
+        <nav className=' w-2/3 border border-b-2 bg-white text-red-400 py-4 rounded-md'>
+          this is the main navbar
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
