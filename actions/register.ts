8affb381037521d73398134f0dc03fb88d4407ca
Royweru <royezi17@gmail.com>
2024-02-05 09:55:20 +0300

    modified:   actions/register.ts 	modified:   components/modal/sign-up-modal.tsx

--- actions/register.ts
+++ actions/register.ts
@@ -1,11 +1,32 @@
 "use server"
 import *as z from "zod"
-
+import bcrypt from "bcrypt"
 import { RegisterSchema } from "@/schemas"
+
+import prisma from "@/lib/prismadb"
+
 export const register =async (values:z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values)
 
     if(!validatedFields.success)return {error:"Invalid Fields!"}
 
-    return {success:"Email sent!"}
+    const{email,name,password} = validatedFields.data
+
+    const hashedPwd = await bcrypt.hash(password,10)
+
+    const exsistingUser = await prisma.user.findUnique({
+        where:{
+            email
+        }
+    })
+    if(exsistingUser)return {error:"Email already exists!"}
+
+    await prisma.user.create({
+        data:{
+            email,
+            password:hashedPwd,
+            name
+        }
+    })
+    return {success:"User created!"}
 }
--- components/modal/sign-up-modal.tsx
+++ components/modal/sign-up-modal.tsx
@@ -1,19 +1,122 @@
 "use client";
-import React from "react";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
+import React, { startTransition, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
 import { useModal } from "@/hooks/use-modal-store";
+import { useForm } from "react-hook-form";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { RegisterSchema } from "@/schemas";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { register } from "@/actions/register";
 
 export const SignUpModal = () => {
+  const [success, setSuccess] = useState<string | undefined>("");
+  const [err, setErr] = useState<string | undefined>("");
   const { isOpen, onClose, type } = useModal();
   const modalOpen = isOpen && type === "sign-up";
 
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (vals: z.infer<typeof RegisterSchema>) => {
+    startTransition(() => {
+      register(vals).then((data) => {
+        setSuccess(data?.success);
+        setErr(data?.error);
+        form.reset();
+      });
+    });
+  };
+
+  const isLoading = form.formState.isSubmitting;
   return (
     <Dialog open={modalOpen} onOpenChange={onClose}>
       <DialogContent className=" bg-zinc-200 overflow-hidden ">
         <DialogHeader>
           <DialogTitle>SIGN UP</DialogTitle>
         </DialogHeader>
-        REG FORM GOES HERE
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div className=" w-full  space-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>NAME</FormLabel>
+                    <FormControl>
+                      <Input placeholder=" Enter your name please" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>EMAIL</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder=" Enter your email please"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>PASSWORD</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder=" Enter your password please"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <div className=" w-full">
+                <Button className=" w-full " type="submit" disabled={isLoading}>
+                  CREATE ACCOUNT
+                </Button>
+              </div>
+            </div>
+          </form>
+        </Form>
+       
+        <DialogFooter>
+        {err && (
+          <div className=" w-full bg-destructive p-3 text-xl font-mono mt-2 font-semibold">
+            {err}
+          </div>
+        )}
+        {success && (
+          <div className=" w-full bg-emerald-300 p-3 text-xl font-mono mt-2 font-semibold">
+            {success}
+          </div>
+        )}
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
